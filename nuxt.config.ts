21509435742d595f669076187dfe901d2d732df3
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2024-09-23 22:17:26 -0500

    chore: bump deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ import {
 
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
+  devtools: { enabled: true, componentInspector: false },
   future: { compatibilityVersion: 4 },
   experimental: { typedPages: true },
   watch: ["locales"],
